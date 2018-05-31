67f2ba0e4939da028b36cba9fa9b29c5068b3200
DufourSy <39744004+DufourSy@users.noreply.github.com>
2018-05-31 19:30:58 +0200

    Add files via upload

--- modele/m_all.php
+++ modele/m_all.php
@@ -1,14 +1,3 @@
 <?php
-    try {
-    //$bdd = new PDO('mysql:dbname=ppe;host=localhost', 'root', '');
-    $bdd = new PDO('mysql:dbname=ppe;host=localhost', 'admin', 'Btssio');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $req = $bdd->prepare('SELECT * FROM evenement,user,lieu WHERE evenement.id_lieu = lieu.id_lieu and evenement.id_user = user.id_user');
-    $req->execute();
-    $resultat = $req->fetchAll();
 
-    }
-    catch (PDOException $e) {
-        die($e->getMessage());
-    }
 ?>
