61bc0c05590b273bd2918a3871faadf7166a8777
DufourSy <39744004+DufourSy@users.noreply.github.com>
2018-05-31 19:24:59 +0200

    Add files via upload

--- vue/v_compte.php
+++ vue/v_compte.php
@@ -1,20 +1,3 @@
-<?php
-
-    try {
-    //$bdd = new PDO('mysql:dbname=ppe;host=localhost', 'root', '');
-    $bdd = new PDO('mysql:dbname=ppe;host=localhost', 'admin', 'Btssio');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $req = $bdd->prepare('SELECT * FROM user WHERE login = :pseudo');
-    $req->execute(array(
-        'pseudo' => $_COOKIE['pseudo']));
-    $resultat = $req->fetch();
-
-    }
-    catch (PDOException $e) {
-        die($e->getMessage());
-    }
-?>
-
 <section id="page_main">
 <aside id="left_panel">
     <div id="live">
